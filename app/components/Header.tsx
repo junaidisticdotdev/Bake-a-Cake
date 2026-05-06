a23f4dd9a81e824a712e379ef0406e63e8e74b0a
Muhammad Junaid <hafizjunaidqazisb@gmail.com>
2026-05-06 11:07:12 +0500

    added new logo and on testing

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -20,11 +20,11 @@ const Header = () => {
     return (
         // Header ko relative aur z-index dein taake ye normal flow mein rahe
         <div className="relative z-50 bg-white">
-            <header className='w-full h-24 flex justify-between items-center px-6 md:px-10 lg:px-20 shadow-sm'>
-                <img src="/Media/logo1.png" alt="Logo" className='h-12 cursor-pointer' />
+            <header className='w-full h-20 sm:h-24 flex justify-between items-center px-4 md:px-10  shadow-sm'>
+                <img src="/Media/logo2.png" alt="Logo" className='h-32 lg:h-40 cursor-pointer' />
 
                 {/* Desktop Menu */}
-                <ul className='hidden md:flex items-center gap-10 text-lg font-medium'>
+                <ul className='hidden md:flex items-center gap-8 text-md font-medium'>
                     {navLinks.map((link) => (
                         <li key={link.name} className='cursor-pointer hover:text-[#f79dc3] duration-300 transition-all font-serif tracking-wide'>
                             <Link href={link.href}>{link.name}</Link>
@@ -33,7 +33,7 @@ const Header = () => {
                     <li>
                         <a href={`https://wa.me/923084978902?text=${encodeURIComponent("Assalam-o-Alaikum! I want to order a Cute Cake.")}`}
                             target="_blank" rel="noopener noreferrer">
-                            <button className="px-6 py-3 bg-[#f79dc3] hover:bg-[#e08bb1] text-white rounded-xl font-bold transition-all duration-300 shadow-md active:scale-95 uppercase tracking-wide text-sm font-serif">
+                            <button className="px-3 py-2 bg-[#f79dc3] hover:bg-[#e08bb1] text-white rounded-xl font-semibold cursor-pointer transition-all duration-300 shadow-md active:scale-95 uppercase tracking-wide text-sm font-serif">
                                 Order Now
                             </button>
                         </a>
@@ -64,16 +64,16 @@ const Header = () => {
                                 animate={{ x: 0 }}
                                 exit={{ x: '100%' }}
                                 transition={{ type: 'spring', damping: 25, stiffness: 200 }}
-                                className="fixed top-0 right-0 h-full w-[85%] sm:w-[60%] bg-white shadow-2xl p-6 z-[110] overflow-y-auto"
+                                className="fixed top-0 right-0 h-full w-[75%] sm:w-[60%] bg-white shadow-2xl p-6 z-[110] overflow-y-auto"
                             >
                                 <div className='flex justify-end mb-10'>
                                     <IoMdClose 
-                                        className='text-4xl cursor-pointer text-slate-800'
+                                        className='text-3xl cursor-pointer text-slate-800'
                                         onClick={closeMenu} 
                                     />
                                 </div>
 
-                                <ul className='flex flex-col gap-8 text-2xl font-medium px-4'>
+                                <ul className='flex flex-col gap-8 text-xxl font-medium px-4'>
                                     {navLinks.map((link) => (
                                         <motion.li 
                                             key={link.name}
@@ -89,7 +89,7 @@ const Header = () => {
                                     <li className='mt-6'>
                                         <a href={`https://wa.me/923084978902?text=${encodeURIComponent("Assalam-o-Alaikum!")}`}
                                             target="_blank" rel="noopener noreferrer" onClick={closeMenu}>
-                                            <button className="w-full py-4 bg-[#f79dc3] text-white rounded-xl font-bold shadow-lg font-serif uppercase tracking-widest active:scale-95 transition-all">
+                                            <button className="w-full py-2 px-2 bg-[#f79dc3] text-white rounded-xl text-md font-semibold shadow-lg font-serif uppercase tracking-wide active:scale-95 transition-all">
                                                 Order Now
                                             </button>
                                         </a>
