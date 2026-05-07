f5a8a3f0cba88d580a923ef7c0510bc77ca6b94f
Muhammad Junaid <hafizjunaidqazisb@gmail.com>
2026-05-07 14:11:37 +0500

    margin set for pages

--- app/module/gallery/GalleryHero.tsx
+++ app/module/gallery/GalleryHero.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const GalleryHero = () => {
   return (
-    <div className='mt-20 sm:mt-0'>
+    <div className='mt-20'>
         <div className="w-full h-16 bg-[#e0f7ff] flex items-center justify-start gap-4 sm:gap-6 pl-6 sm:pl-10">
         <p className="text-sm sm:text-lg md:text-xl italic font-serif cursor-pointer hover:text-[#f79dc3] transition-colors">HOME</p>
         <p className="text-gray-400">-</p>
